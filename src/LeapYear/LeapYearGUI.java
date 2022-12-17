61102925f569cc6ba4751ac8725c08eeca34e51b
Christopher <topitops123@gmail.com>
2022-12-17 14:00:00 +0800

    Changes: I forgot to extends JFrame the LeapYearGUI
    Added Main function in order to view the GUI

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -2,7 +2,7 @@ package LeapYear;
 
 import javax.swing.*;
 
-public class LeapYearGUI {
+public class LeapYearGUI extends JFrame{
     private JPanel pnlMain;
     private JTextField tfYear;
     private JButton btnCheckYear;
@@ -10,7 +10,7 @@ public class LeapYearGUI {
     public LeapYearGUI() {
 
         btnCheckYear.addActionListener((event) -> {
-
+            try {
                 int year = Integer.parseInt(tfYear.getText());
 
                 if (year % 4 == 0) {
@@ -26,9 +26,25 @@ public class LeapYearGUI {
                 } else {
                     JOptionPane.showMessageDialog(pnlMain, "Not Leap Year", "Message", 1);
                 }
-
+            }
+            catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(pnlMain, "Input must be a number. PLease try again.");
+            }
         });
-    }
+
+
 
     }
+    public static void main(String[] args) {
+        LeapYearGUI app = new LeapYearGUI();
+        app.setTitle("Leap year Checker");
+        app.setContentPane(app.pnlMain);
+        app.setSize(700, 700);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        app.setVisible(true);
+
+
+    }
+
+}
 
