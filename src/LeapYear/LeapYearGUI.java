37845f422e0c81bc4aa2a960cbb2b85bd8ee631e
Christopher <topitops123@gmail.com>
2022-12-17 13:53:30 +0800

    Changes: Addedd an addActionListener Event.
    Changed and added a JOptionPane that displays "Leap year" if the input year is a leap year and "Not a leap year" if it is not.

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -6,4 +6,29 @@ public class LeapYearGUI {
     private JPanel pnlMain;
     private JTextField tfYear;
     private JButton btnCheckYear;
-}
+
+    public LeapYearGUI() {
+
+        btnCheckYear.addActionListener((event) -> {
+
+                int year = Integer.parseInt(tfYear.getText());
+
+                if (year % 4 == 0) {
+                    if (year % 100 == 0) {
+                        if (year % 400 == 0) {
+                            JOptionPane.showMessageDialog(pnlMain, "Leap Year", "Message", 1);
+                        } else {
+                            JOptionPane.showMessageDialog(pnlMain, "Not Leap Year", "Message", 1);
+                        }
+                    } else {
+                        JOptionPane.showMessageDialog(pnlMain, "Leap Year", "Message", 1);
+                    }
+                } else {
+                    JOptionPane.showMessageDialog(pnlMain, "Not Leap Year", "Message", 1);
+                }
+
+        });
+    }
+
+    }
+
