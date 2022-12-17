cbe1bb9800cd48112c3a06320c414115871c68df
Christopher <topitops123@gmail.com>
2022-12-17 14:21:53 +0800

    Added the "extends JFrame" so main would function

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -2,11 +2,21 @@ package SimpleCalc;
 
 import javax.swing.*;
 
-public class SimpleCalcGUI {
+public class SimpleCalcGUI extends JFrame{
     private JPanel pnlMain;
     private JTextField tfNumber1;
     private JComboBox comboBox1;
     private JButton btnCompute;
     private JTextField tfNumber2;
     private JTextField lblResult;
+
+    public static void main(String[] args) {
+        SimpleCalcGUI app = new SimpleCalcGUI();
+        app.setTitle("Simple Calculator");
+        app.setContentPane(app.pnlMain);
+        app.setSize(700, 400);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        app.setVisible(true);
+    }
 }
+
