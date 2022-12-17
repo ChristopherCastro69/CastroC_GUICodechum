f4d578c9e2ec42eeb138c10897314e702d6b5a12
Christopher <topitops123@gmail.com>
2022-12-17 14:39:37 +0800

    Added an array list of the discounts and menus. also added a main function in order to view the GUI

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -1,10 +1,33 @@
 package FoodOrdering;
 
 import javax.swing.*;
+import java.util.List;
 
-public class FoodOrderGUI {
-    private JPanel panel1;
-    private JCheckBox pizzaCheckBox;
-    private JRadioButton noneRadioButton;
-    private JButton orderButton;
+public class FoodOrderGUI extends JFrame{
+    private JPanel pnlMain;
+    private JCheckBox cPizza;
+    private JRadioButton rbNone;
+    private JButton btnOrder;
+    private JCheckBox cBurger;
+    private JCheckBox cFries;
+    private JCheckBox cSoftDrinks;
+    private JCheckBox cTea;
+    private JCheckBox cSundae;
+    private JRadioButton rb5;
+    private JRadioButton rb10;
+    private JRadioButton rb15;
+
+    private List<JCheckBox> menu;
+
+    private List<JRadioButton> discounts;
+
+    public static void main(String[] args) {
+        FoodOrderGUI app = new FoodOrderGUI();
+        app.setTitle("Food Ordering System");
+        app.setContentPane(app.pnlMain);
+        app.setSize(600, 600);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        app.setVisible(true);
+    }
+
 }
