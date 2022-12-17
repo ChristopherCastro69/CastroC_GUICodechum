6737fb8aa3eb77783f2c75ac2c14a6710b5d9f73
Christopher <topitops123@gmail.com>
2022-12-17 14:20:24 +0800

    Finished with Leap Year.
    Next us the Simple GUI Calculator
    
    renamed 2 text fields named tfNumber1 and tfNumber2 where users can type in numbers
    
    also renamed 1 label named lblResult which displays the result.
    
    Also renamed pnlMain
    And also renamed the compute button to btnCompute

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -6,7 +6,7 @@ public class SimpleCalcGUI {
     private JPanel pnlMain;
     private JTextField tfNumber1;
     private JComboBox comboBox1;
-    private JButton computeResultButton;
+    private JButton btnCompute;
     private JTextField tfNumber2;
     private JTextField lblResult;
 }
